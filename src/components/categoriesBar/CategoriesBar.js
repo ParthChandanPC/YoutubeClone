6bf44e8d8332dcc9659c02af1c143fdfeb50b0b4
Parth Chandan <parthchandanpc@gmail.com>
2022-07-31 18:13:57 +0530

    Updated components folder

---
+++ src/components/categoriesBar/CategoriesBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./_categoriesBar.scss"
+const CategoriesBar = () => {
+  return (
+    <div>
+      Bar
+    </div>
+  )
+}
+
+export default CategoriesBar
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./_header.scss"
+const Header = () => {
+  return (
+    <div>
+      header
+    </div>
+  )
+}
+
+export default Header
