3985c734bd5eff1e863a78ac123290a54e785fde
Parth Chandan <parthchandanpc@gmail.com>
2022-08-02 19:01:10 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./_base.scss"
-ReactDOM.render(<App />, document.getElementById("root"))
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './App'
+import { Provider } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import './_base.scss'
+import store from './redux/store'
+
+ReactDOM.render(
+   <Provider store={store}>
+      <Router>
+         <App />
+      </Router>
+   </Provider>,
+
+   document.getElementById('root')
+)
