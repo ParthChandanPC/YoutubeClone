a3336b2b4a615b31b83708f01bc31909ca3cb0a7
Parth Chandan <parthchandanpc@gmail.com>
2022-08-02 18:25:07 +0530

    Updated app file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,74 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
-
 import Header from './components/header/Header'
 import Sidebar from './components/sidebar/Sidebar'
 import HomeScreen from './screens/homeScreen/HomeScreen'
 import LoginScreen from './screens/loginScreen/LoginScreen'
-
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom'
 import './_app.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { load_user } from './redux/actions/auth.action'
 
-const App = () => {
+const Layout = ({ children }) => {
    const [sidebar, toggleSidebar] = useState(false)
 
    const handleToggleSidebar = () => toggleSidebar(value => !value)
 
    return (
-      // <>
-      //    <Header handleToggleSidebar={handleToggleSidebar} />
-      //    <div className="app__container">
-      //       <Sidebar
-      //          sidebar={sidebar}
-      //          handleToggleSidebar={handleToggleSidebar}
-      //       />
-      //       <Container fluid className="app__main ">
-      //          <HomeScreen />
-      //    </Container>
-      //    </div>
-      // </>
-      <LoginScreen />
+      <>
+         <Header handleToggleSidebar={handleToggleSidebar} />
+         <div className='app__container'>
+            <Sidebar
+               sidebar={sidebar}
+               handleToggleSidebar={handleToggleSidebar}
+            />
+            <Container fluid className='app__main '>
+               {children}
+            </Container>
+         </div>
+      </>
+   )
+}
+
+const App = () => {
+   const { accessToken, loading } = useSelector(state => state.auth)
+
+   const history = useHistory()
+
+   const dispatch = useDispatch()
+
+   useEffect(() => {
+      dispatch(load_user())
+   }, [dispatch])
+
+   useEffect(() => {
+      if (!loading && !accessToken) {
+         history.push('/auth')
+      }
+   }, [accessToken, loading, history])
+
+   return (
+      <Switch>
+         <Route path='/' exact>
+            <Layout>
+               <HomeScreen />
+            </Layout>
+         </Route>
+
+         <Route path='/auth'>
+            <LoginScreen />
+         </Route>
+
+         <Route path='/search'>
+            <Layout>
+               <h1>Search Results</h1>
+            </Layout>
+         </Route>
+
+         <Route>
+            <Redirect to='/' />
+         </Route>
+      </Switch>
    )
 }
 
