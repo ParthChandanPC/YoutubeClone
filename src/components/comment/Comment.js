d7fae4d6ee7e540b40c2ef396ff19ae2b2570564
Parth Chandan <parthchandanpc@gmail.com>
2022-08-08 21:45:42 +0530

    Updated comment section

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import moment from 'moment'
 import './_comment.scss'
-const Comment = () => {
+const Comment = ({ comment }) => {
+   const {
+      authorDisplayName,
+      authorProfileImageUrl,
+      publishedAt,
+      textDisplay,
+   } = comment
    return (
-      <div className='comment p-2 d-flex'>
+      <div className='p-2 comment d-flex'>
          <img
-            src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png'
+            src={authorProfileImageUrl}
             alt=''
-            className='rounded-circle mr-3'
+            className='mr-3 rounded-circle'
          />
          <div className='comment__body'>
-            <p className='comment__header mb-1'>
-               Parth Chandan • {moment('2022-08-08').fromNow()}
+         <p className='mb-1 comment__header'>
+               {authorDisplayName} • {moment(publishedAt).fromNow()}
             </p>
-            <p className='mb-0'>Nice!</p>
+            <p className='mb-0'>{textDisplay}</p>
          </div>
       </div>
    )
