b2e5fcbb0693a59030e2738b95d04895909135ee
Parth Chandan <parthchandanpc@gmail.com>
2022-08-09 17:13:39 +0530

    Updated horizontal video view

--- src/components/videoHorizontal/VideoHorizontal.js
+++ src/components/videoHorizontal/VideoHorizontal.js
@@ -1,45 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './_videoHorizontal.scss'
-
 import { AiFillEye } from 'react-icons/ai'
 import request from '../../api'
-
 import moment from 'moment'
 import numeral from 'numeral'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { Col, Row } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
+const VideoHorizontal = ({ video }) => {
+   const {
+      id,
+      snippet: {
+         channelId,
+         channelTitle,
+         description,
+         title,
+         publishedAt,
+         thumbnails: { medium },
+      },
+   } = video
 
-const VideoHorizontal = () => {
-   const seconds = moment.duration('100').asSeconds()
+   const [views, setViews] = useState(null)
+   const [duration, setDuration] = useState(null)
+   const [channelIcon, setChannelIcon] = useState(null)
+
+   useEffect(() => {
+      const get_video_details = async () => {
+         const {
+            data: { items },
+         } = await request('/videos', {
+            params: {
+               part: 'contentDetails,statistics',
+               id: id.videoId,
+            },
+         })
+         setDuration(items[0].contentDetails.duration)
+         setViews(items[0].statistics.viewCount)
+      }
+      get_video_details()
+   }, [id])
+
+   useEffect(() => {
+      const get_channel_icon = async () => {
+         const {
+            data: { items },
+         } = await request('/channels', {
+            params: {
+               part: 'snippet',
+               id: channelId,
+            },
+         })
+         setChannelIcon(items[0].snippet.thumbnails.default)
+      }
+      get_channel_icon()
+   }, [channelId])
+
+   const seconds = moment.duration(duration).asSeconds()
    const _duration = moment.utc(seconds * 1000).format('mm:ss')
+   const history = useHistory()
+   const handleClick = () => {
+      history.push(`/watch/${id.videoId}`)
+   }
 
    return (
-      <Row className='videoHorizontal m-1 py-2 align-align-items-center'>
-         <Col xs={6} md={4} className='videoHorizontal__left'>
+      <Row
+         className='py-2 m-1 videoHorizontal align-align-items-center'
+         onClick={handleClick}>
+         <Col xs={6} md={6} className='videoHorizontal__left'>
             <LazyLoadImage
-               src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png'
+               src={medium.url}
                effect='blur'
                className='videoHorizontal__thumbnail'
                wrapperClassName='videoHorizontal__thumbnail-wrapper'
             />
-            <span className='video__top__duration'>{_duration}</span>
+            <span className='videoHorizontal__duration'>{_duration}</span>
          </Col>
-         <Col xs={6} md={8} className='videoHorizontal__right p-0'>
-            <p className='videoHorizontal__title mb-1'>
-               Want to Master Machine Learning?
-            </p>
+         <Col xs={6} md={6} className='p-0 videoHorizontal__right'>
+            <p className='mb-1 videoHorizontal__title'>{title}</p>
             <div className='videoHorizontal__details'>
-               <AiFillEye /> {numeral(1000000).format('0.a')} Views •
-               {moment('2020-06-09').fromNow()}
+            <AiFillEye /> {numeral(views).format('0.a')} Views •
+               {moment(publishedAt).fromNow()}
             </div>
-
-            <div className='videoHorizontal__channel d-flex align-items-center my-1'>
-               {/* <LazyLoadImage
-               src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png'
-               effect='blur'
-             
-            /> */}
-               <p>Parth Chandan</p>
+            <div className='my-1 videoHorizontal__channel d-flex align-items-center'>
+               <p className='mb-0'>{channelTitle}</p>
             </div>
          </Col>
       </Row>
